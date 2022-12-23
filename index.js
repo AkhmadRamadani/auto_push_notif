f1a9b30680450a958d83d5d64c2f9190256220d2
Akhmad Ramadani <akhmdramadani18@gmail.com>
2022-12-23 10:29:47 +0700

    fix send to topic not fcm

--- index.js
+++ index.js
@@ -14,120 +14,146 @@ const db = admin.firestore();
 // get the tokens from the firestore users collection
 const topicDrinkReminder = "drinkReminder";
 const topicFillJournal = "fillJournal";
-// const tokens = db.collection('users').get().then((snapshot) => {
-//     snapshot.forEach((doc) => {
-//         // console.log(doc.id, '=>', doc.data());
-//         return doc.data().token;
-//     });
-// });
 
 cron.schedule('0 9 * * *', () => {
-    /// get user id from firestore
+    const data = {
+        notification: {
+            title: 'Good Morning! Have a Nice Day',
+            body: 'Start your day with a glass of water',
+        },
+        data: {
+            click_action: 'FLUTTER_NOTIFICATION_CLICK',
+        },
+        topic: topicDrinkReminder,
+    };
+
+    sendNotification(data);
+
     db.collection('users').get().then((snapshot) => {
         snapshot.forEach((doc) => {
-
-            const morningMessage = {
-                notification: {
-                    title: 'Good Morning! Have a Nice Day',
-                    body: 'Start your day with a glass of water',
-                },
-                data: {
-                    click_action: 'FLUTTER_NOTIFICATION_CLICK',
-                    userId: doc.id,
-
-                },
-
+            db.collection('notifications').add({
+                title: data.notification.title,
+                body: data.notification.body,
+                userId: doc.id,
+                date: new Date(),
                 topic: topicDrinkReminder,
-            };
-            sendNotification(morningMessage);
-
+            });
         });
     });
+
 
 });
 
 
 cron.schedule('0 12 * * *', () => {
+    const data = {
+        notification: {
+            title: "It's time to take a break",
+            body: 'Have a glass of water and keep hydrated',
+        },
+        data: {
+            click_action: 'FLUTTER_NOTIFICATION_CLICK',
+        },
+        topic: topicDrinkReminder,
+    };
+
+    sendNotification(data);
+
     db.collection('users').get().then((snapshot) => {
         snapshot.forEach((doc) => {
-
-            const lunchMessage = {
-                notification: {
-                    title: "It's time to take a break",
-                    body: 'Have a glass of water and keep hydrated',
-                },
-                data: {
-                    click_action: 'FLUTTER_NOTIFICATION_CLICK',
-                    userId: doc.id,
-                },
-
+            db.collection('notifications').add({
+                title: data.notification.title,
+                body: data.notification.body,
+                userId: doc.id,
+                date: new Date(),
                 topic: topicDrinkReminder,
-            };
-            sendNotification(lunchMessage);
-
+            });
         });
-    }
-    );
+    });
+
 });
 
 cron.schedule('0 17 * * *', () => {
+
+    const data = {
+        notification: {
+            title: "Wow! You've done a great job today",
+            body: "Keep hydrated and don't forget to fill your journal",
+        },
+        data: {
+            click_action: 'FLUTTER_NOTIFICATION_CLICK',
+        },
+        topic: topicDrinkReminder,
+    };
+
+    sendNotification(data);
+
     db.collection('users').get().then((snapshot) => {
         snapshot.forEach((doc) => {
-
-            const dinnerMessage = {
-                notification: {
-                    title: "Wow! You've done a great job today",
-                    body: "Keep hydrated and don't forget to fill your journal",
-                },
-                data: {
-                    click_action: 'FLUTTER_NOTIFICATION_CLICK',
-                    userId: doc.id,
-                },
-                
+            db.collection('notifications').add({
+                title: data.notification.title,
+                body: data.notification.body,
+                userId: doc.id,
+                date: new Date(),
                 topic: topicDrinkReminder,
-            };
-            sendNotification(dinnerMessage);
-
+            });
         });
-    }
-    );
+    });
+
 });
+
 cron.schedule('0 20 * * *', () => {
+
+    const data = {
+        notification: {
+            title: "How was your day?",
+            body: "We hope you had a great day. Don't forget to fill your journal",
+        },
+        data: {
+            click_action: 'FLUTTER_NOTIFICATION_CLICK',
+        },
+        topic: topicFillJournal,
+    };
+
+    sendNotification(data);
+
     db.collection('users').get().then((snapshot) => {
         snapshot.forEach((doc) => {
-
-            const dinnerMessage = {
-                notification: {
-                    title: "How was your day?",
-                    body: "We hope you had a great day. Don't forget to fill your journal",
-                },
-                data: {
-                    click_action: 'FLUTTER_NOTIFICATION_CLICK',
-                    userId: doc.id,
-                },
-                
+            db.collection('notifications').add({
+                title: data.notification.title,
+                body: data.notification.body,
+                userId: doc.id,
+                date: new Date(),
                 topic: topicFillJournal,
-            };
-            sendNotification(dinnerMessage);
+            });
         });
     });
 });
 
 cron.schedule('0 22 * * *', () => {
+
+    const data = {
+        notification: {
+            title: 'Good Night! Have a Nice Dream',
+            body: 'Rest well and have a glass of water before you sleep',
+        },
+        data: {
+            click_action: 'FLUTTER_NOTIFICATION_CLICK',
+        },
+        topic: topicDrinkReminder,
+    };
+
+    sendNotification(data);
+
     db.collection('users').get().then((snapshot) => {
         snapshot.forEach((doc) => {
-            const dinnerMessage = {
-                notification: {
-                    title: 'Good Night! Have a Nice Dream',
-                    body: 'Rest well and have a glass of water before you sleep',
-                },
-                data: {
-                    click_action: 'FLUTTER_NOTIFICATION_CLICK',
-                    userId: doc.id,
-                },
+            db.collection('notifications').add({
+                title: data.notification.title,
+                body: data.notification.body,
+                userId: doc.id,
+                date: new Date(),
                 topic: topicDrinkReminder,
-            };
-            sendNotification(dinnerMessage);
+            });
         });
     });
 });
@@ -136,13 +162,6 @@ cron.schedule('0 22 * * *', () => {
 function sendNotification(data) {
     admin.messaging().send(data)
         .then((response) => {
-            db.collection('notifications').add({
-                title: data.notification.title,
-                body: data.notification.body,
-                userId: data.data.userId,
-                date: new Date(),
-            });
-
             console.log(`Successfully sent message: ${response}`);
         })
         .catch((error) => {
